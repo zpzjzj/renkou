7644aed966ddc07e44e610e48686373c52911b9d
CHEN YUJIA <nearc116@hotmail.com>
2014-04-03 09:11:12 +0800

    [VERSION 0.1] can do: 1. add/remove StudyObject 2. load xml file to display dataitems
    to do:
    1. load parameter
    2. detect duplicate StudyObject
    3. remove temp file after comliting

--- ui-wizard/newprojection/EnumClass.h
+++ ui-wizard/newprojection/EnumClass.h
@@ -4,6 +4,11 @@
 namespace EnumClass {
 const int FROM_YEAR = 1980;
 const int TO_YEAR = 2050;
+const QString WIN_PREFIX = "D:\\";
+const QString MACOS_PREFIX= "/Users/nearc/";
+const QString PREFIX = MACOS_PREFIX;
+
+
 const QList<QString> ProvinceList={
   "北京市", "天津市", "河北省", "陕西省", "内蒙古", "辽宁省", "吉林省",
     "黑龙江"
